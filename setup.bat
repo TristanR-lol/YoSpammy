d3d067bc7bd59dd1c17ac388fcbea4579d5d2d1d
Bloxer126g <tristan.raneroyay@proton.me>
2026-02-22 16:40:34 +0000

    v1.0.0 2 tab

--- setup.bat
+++ setup.bat
@@ -1,3 +1,22 @@
+@echo off
+setlocal
+
+:: Find the python executable and store it
+for /f "delims=" %%i in ('where python') do (
+    set PYTHON=%%i
+    goto :found
+)
+
+:notfound
+echo Python not found. Installing...
 winget install -e --id Python.Python.3.11 --scope machine
-python -m pip install customtkinter pyautogui pynput
-python -m main.py
+:: Refresh PATH after install
+for /f "delims=" %%i in ('where python') do (
+    set PYTHON=%%i
+    goto :found
+)
+
+:found
+echo Using Python: %PYTHON%
+"%PYTHON%" -m pip install customtkinter pyautogui win11toast pynput
+"%PYTHON%" main.py
