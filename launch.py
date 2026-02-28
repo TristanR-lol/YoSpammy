2d7cba30615c7f96ea61af3fe43e179069196170
Bloxer126g <tristan.raneroyay@proton.me>
2026-02-28 15:26:55 +0000

    Add auto update for setup.bat

--- launch.py
+++ launch.py
@@ -1,16 +1,14 @@
-from customtkinter import *
 from subprocess import Popen
 from requests import get
-root = CTk()
 
-TaskLabel  = CTkLabel(root, text="Updating!")
-TaskLabel.pack()
-
-UpdateLink = "https://raw.githubusercontent.com/TristanR-lol/YoSpammy/refs/heads/main/main.pyw"
-NewContent = get(UpdateLink)
+UpdateLink1 = "https://raw.githubusercontent.com/TristanR-lol/YoSpammy/refs/heads/main/main.pyw"
+UpdateLink2 = "https://raw.githubusercontent.com/TristanR-lol/YoSpammy/refs/heads/main/setup.bat"
+NewContent1 = get(UpdateLink1)
+NewContent2 = get(UpdateLink2)
 
 with open("main.pyw", "w", encoding="utf-8") as f:
-  f.write(NewContent.text)
+  f.write(NewContent1.text)
+with open("setup.bat", "w", encoding="utf-8") as f:
+  f.write(NewContent2.text)
 
 Popen(['pythonw', 'main.pyw'])   
-root.mainloop()
