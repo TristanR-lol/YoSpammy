49d6bb6f66ede69372f29b7ff5084731a98f28cd
Bloxer126g <tristan.raneroyay@proton.me>
2026-02-28 15:31:59 +0000

    UNDO

--- launch.py
+++ launch.py
@@ -9,8 +9,5 @@ NewContent2 = get(UpdateLink2)
 with open("setup.bat", "w", encoding="utf-8") as f:
   f.write(NewContent2.text)
 with open("main.pyw", "w", encoding="utf-8") as f:
-  if f.read() != NewContent1:
-    f.write(NewContent1.text)
-    Popen(["setup.bat"], shell=True)
-  else:
-    Popen(['pythonw', 'main.pyw']) 
+  f.write(NewContent1.text)
+Popen(['pythonw', 'main.pyw'])
